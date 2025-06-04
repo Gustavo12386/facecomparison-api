70d1763a5cb5ee012d2abdaf7992c652987f94ee
Gustavo12386 <gucalderaro@hotmail.com>
2025-06-04 13:53:14 -0300

    fix test spring

--- src/test/java/com/br/hub/facecomparsion/FacecomparsionApplicationTests.java
+++ src/test/java/com/br/hub/facecomparsion/FacecomparsionApplicationTests.java
@@ -1,15 +1,21 @@
 package com.br.hub.facecomparsion;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import com.br.hub.facecomparsion.config.FacecomparsionApplication;
 
 @SpringBootTest(classes = FacecomparsionApplication.class)
-@ActiveProfiles(value = "test")
 class FacecomparsionApplicationTests {
 
+	@BeforeAll
+	static void setupProperties() {
+		System.setProperty("aws.region", "us-east-1");
+		System.setProperty("aws.accessKeyId", "localstack");
+		System.setProperty("aws.secretAccessKey", "localstack");
+	}
+	
 	@Test
 	void contextLoads() {
 	}
